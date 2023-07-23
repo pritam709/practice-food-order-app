d761baecb6db525e3ea1249c5332f7a2ddfea4a7
Pravesh <pritamyadav70923@gmail.com>
2023-07-23 23:37:57 +0530

    updated cart logic

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -4,10 +4,46 @@ import { useContext } from "react"
 import CartContext from "../store/CartContext"
 const Cart=(props)=>{
 
+    const MealsArray = [
+        {
+          id: "m1",
+          name: "Sushi",
+          description: "Finest fish and veggies",
+          price: 22.99,
+          amount:1
+        },
+        {
+          id: "m2",
+          name: "Schnitzel",
+          description: "A german specialty!",
+          price: 16.5,
+          amount:1
+        }]
+
   const ctx=  useContext(CartContext);
 
   return  <Modal onClick={props.onClick}><div className={classes.cart}>
-  Sushi
+ <div>
+    <ul className={classes.list}>
+    {MealsArray.map(item=><li>
+    <div className={classes['list-items']}>
+    <div className={classes.item}>
+        <p>{item.name}</p>
+        <span className={classes.price}>{item.price}</span> <span className={classes.amount}>x{item.amount}</span>
+    </div>
+    <div className={classes.controls}>
+        <button>
+        +
+        </button>
+        <button>-</button>
+    </div>
+    </div>
+    
+        
+    </li>)}
+
+    </ul>
+ </div>
   <div className={classes.total}>
       <span>Total Amount</span>
       <span>{ctx.totalAmount}</span>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,9 +1,32 @@
+import { useReducer } from "react";
 import CartContext from "./CartContext";
 
 const CartProvider = (props) => {
-    const cartContext={
+
+    const defaultCart={
         items:[],
         totalAmount:0
+    }
+
+    const cartReducer=(state,action)=>{
+        return defaultCart;
+    }
+
+   const[cartState,dispatchCartAction]= useReducer(cartReducer,defaultCart);
+
+    const addItemTocart=(item)=>{
+
+        dispatchCartAction({type:"ADD",item:item})
+
+    }
+    const removeItemFromCart=(id)=>{
+
+    }
+    const cartContext={
+        items:cartState.items,
+        totalAmount:cartState.totalAmount,
+        addItem:addItemTocart,
+        removeItem:removeItemFromCart,
     }
   return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
 };
