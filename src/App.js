4000002c063b7286aca22f09e525ba7722410b8d
Pravesh <pritamyadav70923@gmail.com>
2023-07-23 11:37:03 +0530

    meals added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+
 import Header from "./components/Header";
+import Meals from "./components/Meals";
+import Summary from "./components/Summary";
 
 function App() {
   return (
     <div>
       <Header />
+      <Summary />
+      <Meals/>
     </div>
   );
 }
---
+++ src/components/Meals.js
@@ -0,0 +1,46 @@
+import MealItem from "./MealItem";
+import classes from "./Meals.module.css";
+const Meals = () => {
+  const MealsArray = [
+    {
+      id: "m1",
+      name: "Sushi",
+      description: "Finest fish and veggies",
+      price: 22.99,
+    },
+    {
+      id: "m2",
+      name: "Schnitzel",
+      description: "A german specialty!",
+      price: 16.5,
+    },
+    {
+      id: "m3",
+      name: "Barbecue Burger",
+      description: "American, raw, meaty",
+      price: 12.99,
+    },
+    {
+      id: "m4",
+      name: "Green salad",
+      description: "Healthy...and green...",
+      price: 18.99,
+    },
+  ];
+
+  return (
+    <div className={classes.meals}>
+      <ul>
+        {MealsArray.map((item) => (
+          <MealItem
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Meals;
